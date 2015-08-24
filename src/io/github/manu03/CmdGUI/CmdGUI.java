10c77ef5a36d97096cb16cc4efb18b3682d1bfc0
manu-03 <manu80956@gmail.com>
2015-08-24 09:51:57 +0200

    First Build
    Compile first project JAR

---
+++ src/io/github/manu03/CmdGUI/CmdGUI.java
@@ -0,0 +1,15 @@
+package io.github.manu03.CmdGUI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CmdGUI extends JavaPlugin {
+	@Override
+	public void onEnable() {
+		//getLogger().info(arg0);
+	}
+	
+	@Override
+	public void onDisable() {
+		//getLogger().info(arg0);
+	}
+}
